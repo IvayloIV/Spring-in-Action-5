603dccc0aa70b419cdf257c32fc93cb98b87837d
IvayloIV <ivvo98@abv.bg>
2021-03-20 17:30:11 +0200

    Add 5th topic

---
+++ 5.Working_with_configuration_properties/taco-cloud-log/src/main/java/sia/tacocloud/controllers/RegistrationController.java
@@ -0,0 +1,53 @@
+package sia.tacocloud.controllers;
+
+import javax.validation.Valid;
+
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import lombok.extern.slf4j.Slf4j;
+import sia.tacocloud.domain.RegistrationForm;
+import sia.tacocloud.domain.User;
+import sia.tacocloud.repositories.UserRepository;
+
+@Slf4j
+@Controller
+@RequestMapping("/register")
+public class RegistrationController {
+
+    private final PasswordEncoder passwordEncoder;
+    private final UserRepository userRepository;
+
+    public RegistrationController(PasswordEncoder passwordEncoder,
+                                    UserRepository userRepository) {
+        this.passwordEncoder = passwordEncoder;
+        this.userRepository = userRepository;
+    }
+
+    @ModelAttribute(name = "registrationForm")
+    public RegistrationForm registrationFrom() {
+        return new RegistrationForm();
+    }
+
+    @GetMapping
+    public String showRegisterForm() {
+        return "user/register";
+    }
+    
+    @PostMapping
+    public String processRegistration(@Valid RegistrationForm registrationFrom, Errors errors) {
+        log.info(registrationFrom.toString());
+        if (errors.hasErrors()) {
+            return "user/register";
+        }
+
+        User user = registrationFrom.toUser(this.passwordEncoder);
+        this.userRepository.save(user);
+        return "redirect:/login";
+    }
+}
---
+++ 5.Working_with_configuration_properties/taco-cloud-ssl/src/main/resources/sql/data.sql
@@ -0,0 +1,24 @@
+delete from Taco_Order_Tacos;
+delete from Taco_Ingredients;
+delete from Taco;
+delete from Taco_Order;
+delete from Ingredient;
+
+insert into Ingredient (id, name, type)
+ values ('1', 'Orange', 'FRUIT');
+insert into Ingredient (id, name, type)
+ values ('2', 'Banana', 'FRUIT');
+insert into Ingredient (id, name, type)
+ values ('3', 'French', 'CHEESE');
+insert into Ingredient (id, name, type)
+ values ('4', 'Italian', 'CHEESE');
+insert into Ingredient (id, name, type)
+ values ('5', 'Sliced', 'PATATOES');
+insert into Ingredient (id, name, type)
+ values ('6', 'Roasted', 'PATATOES');
+insert into Ingredient (id, name, type)
+ values ('7', 'Pork', 'MEAT');
+insert into Ingredient (id, name, type)
+ values ('8', 'Chicken', 'MEAT');
+ insert into Ingredient (id, name, type)
+ values ('9', 'French', 'CHEESE');
